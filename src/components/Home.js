a6a6449c901a807d9fc68dae0476af6d15922eba
Adar Sönmez <adarsonmez@outlook.com>
2021-09-18 23:07:23 +0300

    Home component added

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section className="home">
+      <h2 className="hello">Hi, I'm Adar 👋</h2>
+      <p className="short-description">
+        Building digital products, brands, and experience
+      </p>
+      <div className="icons">
+        <i class="bi bi-linkedin"></i>
+        <i class="bi bi-github"></i>
+        <i class="bi bi-twitter"></i>
+        <i class="bi bi-envelope"></i>
+        <i class="bi bi-telephone-forward"></i>
+      </div>
+    </section>
+  )
+}
+
+export default Home
