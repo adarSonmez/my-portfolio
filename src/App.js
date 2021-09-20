b34c840ce79fc77e7b1ba55de5ef73afded4fd32
Adar Sönmez <adarsonmez@outlook.com>
2021-09-20 23:50:01 +0300

    AOS package imported and initialized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Aos from 'aos'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import '../node_modules/aos/dist/aos.css'
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
+
   return (
     <React.Fragment>
       <Home />
-      <About />
+      <About/>
       <Skills />
       <Projects />
       <Contact />
