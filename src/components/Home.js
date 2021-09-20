2ed5bb39fa06f6387f95d9fb187f2c707c833d93
Adar Sönmez <adarsonmez@outlook.com>
2021-09-20 23:51:17 +0300

    Social links added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Header from './Header'
 
 function Home(props) {
+  const myGithub = 'https://github.com/adarSonmez'
+  const myTwitter = 'https://twitter.com/adarsonm3z'
+  const myLinkedIn = 'https://www.linkedin.com/in/adar-sonmez-0011110000110011/'
+  const myEmail = 'adarsonmez@outlook.com'
+
   const whoAreYou = (e) => {
-    e.target.innerText = 'I develop software'
+    e.target.innerText = '2A15J78'
     e.target.classList.remove('fade')
-    e.target.classList.add('what-do-you-do')
+    e.target.classList.add('secret')
   }
 
   return (
@@ -14,13 +19,21 @@ function Home(props) {
       <h2 className="iam fade" onClick={(e) => whoAreYou(e)}>
         Hi, I'am Adar.
       </h2>
-      <h2 className="hello">It's great to see you.❤️</h2>
+      <h2 className="its-great">It's great to see you.❤️</h2>
 
-      <div className="icons">
-        <i className="bi bi-linkedin"></i>
-        <i className="bi bi-github"></i>
-        <i className="bi bi-twitter"></i>
-        <i className="bi bi-envelope"></i>
+      <div className="icons" data-aos="fade-up">
+        <a href={myLinkedIn} target="_blank" rel="noreferrer">
+          <i className="bi bi-linkedin"></i>
+        </a>
+        <a href={myGithub} target="_blank" rel="noreferrer">
+          <i className="bi bi-github"></i>
+        </a>
+        <a href={myTwitter} target="_blank" rel="noreferrer">
+          <i className="bi bi-twitter"></i>
+        </a>
+        <a href={'mailto:' + myEmail} target="_blank" rel="noreferrer">
+          <i className="bi bi-envelope"></i>
+        </a>
       </div>
     </section>
   )
