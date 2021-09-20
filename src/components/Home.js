627995a78599d0a9ac941ba1ad0305f933570dfc
Adar Sönmez <adarsonmez@outlook.com>
2021-09-20 16:04:35 +0300

    Cool animations added to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,18 @@ import React from 'react'
 import Header from './Header'
 
 function Home(props) {
+  const whoAreYou = (e) => {
+    e.target.innerText = 'I develop software'
+    e.target.classList.remove('fade')
+    e.target.classList.add('what-do-you-do')
+  }
+
   return (
     <section className="home">
       <Header />
-      <h2 className="iam">Hi, I'am Adar. </h2>
+      <h2 className="iam fade" onClick={(e) => whoAreYou(e)}>
+        Hi, I'am Adar.
+      </h2>
       <h2 className="hello">It's great to see you.❤️</h2>
 
       <div className="icons">
