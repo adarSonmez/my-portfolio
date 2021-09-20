75f522c628a12d51a4bbda57117be11170ad9c91
Adar Sönmez <adarsonmez@outlook.com>
2021-09-20 23:53:41 +0300

    I introduced myself

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,45 @@
 import React from 'react'
 import itsMe from '../asserts/images/leon.jpg'
 
-function About() {
+function About(props) {
   return (
     <section className="about">
-      <div className="img-container">
+      <h2>About Me</h2>
+      <div className="its-me" data-aos="fade-right">
         <img src={itsMe} alt="Adar Sönmez" />
       </div>
-      <h1 className="about-title">About Me</h1>
-      <p className="about-content">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-        <br />
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
-        <br />
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
-        <br />
-      </p>
+      <div className="about-content" data-aos="fade-left">
+        <p>
+          Adar Sönmez was born in 2000 in the World. He loves programming,
+          reading and daydreaming. His favorite website is
+          <a
+            href="https://en.wikipedia.org/wiki/Main_Page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {' '}
+            Wikipedia
+          </a>
+          . His favorite band is<span className="pink"> Pink Floyd.</span>
+        </p>
+        <p>
+          Adar is currently studying Computer Engineering at Eskişehir Technical
+          University. Besides, he is the Avionics Manager of{' '}
+          <a
+            href="https://www.linkedin.com/company/arcturus-rocket-team/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Arcturus Rocket Team.{' '}
+          </a>
+          But his real passion is Web Programming. He has zero years of
+          experience as a web developer in any company.
+        </p>
+        <p>
+          He is specialized in HTML/CSS and JavaScript on the frontend, but He
+          is pretty comfortable with ReactJS, NodeJS and MongoDB.
+        </p>
+      </div>
     </section>
   )
 }
