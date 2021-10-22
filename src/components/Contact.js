e6dd6128c91d99b1eb6824b9afee15c7f9a1e782
Adar Sönmez <adarsonmez@outlook.com>
2021-10-22 13:33:29 +0300

    Footer created

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
 function Contact() {
-  const myGithub = 'https://github.com/adarSonmez'
-  const myTwitter = 'https://twitter.com/adarsonm3z'
-  const myLinkedIn = 'https://www.linkedin.com/in/adar-sonmez-0011110000110011/'
-  const myEmail = 'adarsonmez@outlook.com'
-
   return (
     <section className="contact" id="contact">
       <h2>Contact</h2>
 
       <form className="contact-form">
-      <caption>If you like my work and you have a cool project to work on, you can contact me by filling out the form below.</caption>
+        <caption>
+          If you like my work and you have some cool project to work on, you can
+          contact me by filling out the form below or through my social media
+          accounts listed in the footer.
+        </caption>
         <input type="name" placeholder="name" />
         <input type="email" placeholder="email" />
         <textarea placeholder="content" />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,41 @@
 import React from 'react'
 
 function Footer() {
+  const myGithub = 'https://github.com/adarSonmez'
+  const myTwitter = 'https://twitter.com/adarsonm3z'
+  const myLinkedIn = 'https://www.linkedin.com/in/adar-sonmez-0011110000110011/'
+  const myEmail = 'adarsonmez@outlook.com'
+
   return (
     <footer>
-      This site was built with great tools like ReactJS, VS Code, npm.
+      <div className="icons">
+        <a href={myLinkedIn} target="_blank" rel="noreferrer">
+          <i className="bi bi-linkedin"></i>
+        </a>
+        <a href={myGithub} target="_blank" rel="noreferrer">
+          <i className="bi bi-github"></i>
+        </a>
+        <a href={myTwitter} target="_blank" rel="noreferrer">
+          <i className="bi bi-twitter"></i>
+        </a>
+        <a href={'mailto:' + myEmail} target="_blank" rel="noreferrer">
+          <i className="bi bi-envelope"></i>
+        </a>
+      </div>
+
+      <address>
+        ©{new Date().getFullYear()} ADAR SÖNMEZ. All Rights Reserved.
+        <p className="developer">
+          Developed by{' '}
+          <a
+            href="https://www.linkedin.com/in/adar-sonmez-0011110000110011"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Adar SÖNMEZ
+          </a>
+        </p>
+      </address>
     </footer>
   )
 }
