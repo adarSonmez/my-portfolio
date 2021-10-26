a4c3166a334cc1507abaa6e50df37e8fe130eef3
Adar Sönmez <adarsonmez@outlook.com>
2021-10-26 13:53:23 +0300

    I added my photo

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import itsMe from '../asserts/images/leon.jpg'
+import itsMe from '../asserts/images/me1.jpeg'
 
 function About(props) {
   return (
@@ -10,7 +10,8 @@ function About(props) {
       </div>
       <div className="about-content" data-aos="fade-left">
         <p>
-          Adar Sönmez was born in 2000 in the World. He loves programming,
+          Adar Sönmez was born in 2000 in the World. His job is to turn complex
+          problems into a simple and beautiful designs. He loves programming,
           reading and daydreaming. His favorite website is
           <a
             href="https://en.wikipedia.org/wiki/Main_Page"
@@ -24,12 +25,14 @@ function About(props) {
         </p>
         <p>
           Adar is currently studying Computer Engineering at Eskişehir Technical
-          University. His real passion is Web Programming. He has zero years of
-          experience as a web developer in any company.
+          University. He is a very curious person, so he is never satisfied with
+          what is told to him at school. That's why he always does research and
+          tries to go deeper.
         </p>
         <p>
           He is specialized in HTML/CSS and JavaScript on the frontend, but He
-          is pretty comfortable with ReactJS, NodeJS and MongoDB.
+          is pretty comfortable with ReactJS, NodeJS and MongoDB. He has zero
+          years of experience as a web developer in any company.
         </p>
       </div>
     </section>
